50adc280deea70a4fddbf865220b847cfead84cb
Faith <faithdanghuy@gmail.com>
2024-04-20 17:14:29 +0700

    Minor graphics update

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_SALAMENCE,
+    SPECIES_CHARMANDER,
+    SPECIES_SQUIRTLE,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
